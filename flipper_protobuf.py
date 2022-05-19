a191b6670c538a3e0c266e35da56da06f9f7d14d
SG <who.just.the.doctor@gmail.com>
2022-05-19 13:20:26 +0300

    Comments

--- flipper_protobuf.py
+++ flipper_protobuf.py
@@ -15,7 +15,8 @@ class ProtoFlipper:
         self._serial = serial
         self._command_id = 0
 
-    def read_varint_32(self):
+    def _read_varint_32(self):
+        """Read varint from serial port"""
         MASK = (1 << 32) - 1
 
         result = 0
@@ -34,11 +35,13 @@ class ProtoFlipper:
                     'Too many bytes when decoding varint.')
 
     def _get_command_id(self):
+        """Increment and get command id"""
         self._command_id += 1
         result = self._command_id
         return result
 
     def _cmd_send(self, cmd_data, cmd_name, has_next=False):
+        """Send command"""
         flipper_message = flipper_pb2.Main()
         flipper_message.command_id = self._get_command_id()
         flipper_message.command_status = flipper_pb2.CommandStatus.Value('OK')
@@ -48,7 +51,13 @@ class ProtoFlipper:
                                       ) + flipper_message.SerializeToString())
         self._serial.write(data)
 
+    def _cmd_send_and_read_answer(self, cmd_data, cmd_name, has_next=False):
+        """Send command and read answer"""
+        self._cmd_send(cmd_data, cmd_name, has_next)
+        return self._cmd_read_answer()
+
     def _cmd_read_answer(self, command_id=None):
+        """Read answer from serial port and filter by command id"""
         if command_id == None:
             command_id = self._command_id
 
@@ -59,37 +68,42 @@ class ProtoFlipper:
         return data
 
     def _cmd_read_any(self):
-        length = self.read_varint_32()
+        """Read answer from serial port"""
+        length = self._read_varint_32()
         data = flipper_pb2.Main()
         data.ParseFromString(self._serial.read(size=length))
         return data
 
     def cmd_system_ping(self, data=bytes([0xde, 0xad, 0xbe, 0xef])):
+        """Ping flipper"""
         cmd_data = system_pb2.PingRequest()
         cmd_data.data = data
-        self._cmd_send(cmd_data, 'system_ping_request')
-        data = self._cmd_read_answer()
+        data = self._cmd_send_and_read_answer(cmd_data, 'system_ping_request')
         return data.system_ping_response.data
 
     def cmd_system_audiovisual_alert(self):
+        """Launch audiovisual alert on flipper"""
         cmd_data = system_pb2.PlayAudiovisualAlertRequest()
-        self._cmd_send(cmd_data, 'system_play_audiovisual_alert_request')
-        data = self._cmd_read_answer()
+        data = self._cmd_send_and_read_answer(
+            cmd_data, 'system_play_audiovisual_alert_request')
         return data
 
     def cmd_gui_start_screen_stream(self):
+        """Start screen stream"""
         cmd_data = gui_pb2.StartScreenStreamRequest()
-        self._cmd_send(cmd_data, 'gui_start_screen_stream_request')
-        data = self._cmd_read_answer()
+        data = self._cmd_send_and_read_answer(
+            cmd_data, 'gui_start_screen_stream_request')
         return data
 
     def cmd_gui_stop_screen_stream(self):
+        """Stop screen stream"""
         cmd_data = gui_pb2.StopScreenStreamRequest()
-        self._cmd_send(cmd_data, 'gui_stop_screen_stream_request')
-        data = self._cmd_read_answer()
+        data = self._cmd_send_and_read_answer(
+            cmd_data, 'gui_stop_screen_stream_request')
         return data
 
     def cmd_gui_snapshot_screen(self):
+        """Snapshot screen"""
         self.cmd_gui_start_screen_stream()
         data = self._cmd_read_answer(0)
         self.cmd_gui_stop_screen_stream()
